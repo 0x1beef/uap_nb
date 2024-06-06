1f5fdfa853c74b49c646d2be77f00db158404048
0x1beef <156372403+0x1beef@users.noreply.github.com>
2024-06-06 09:04:16 -0500

    Less verbose frame extract output by default

--- nb/common.py
+++ nb/common.py
@@ -6,6 +6,7 @@ class FrameSequence:
         self.fps = fps
         import glob
         self.num_frames = len(glob.glob(f'{self.frames_dir}/frame_*.png'))
+        print(f"'{frames_dir}': {self.num_frames} frames @ {self.fps:.2f} fps")
     def get_frame(self, frame):
         import cv2
         return cv2.imread(f'{self.frames_dir}/frame_{frame:04d}.png', cv2.IMREAD_GRAYSCALE)
@@ -22,36 +23,39 @@ def get_fps(video):
     fps_str = subprocess.check_output(shlex.split(command)).decode("utf-8").strip()
     return float(fractions.Fraction(fps_str))
     
-def extract_frames(video, frames_dir, filters):
+def extract_frames(video, frames_dir, filters, verbose):
     if not os.path.exists(f'{frames_dir}/frame_0000.png'):
         # note: without the variable frame rate option this would produce some duplicate frames
         output_frames = f'-vsync vfr -start_number 0 {frames_dir}/frame_%04d.png'
-        os.system(f'ffmpeg -i "{video}" -vf "{filters}" {output_frames}')
+        logs = '-hide_banner -loglevel error' if not verbose else ''
+        cmd = f'ffmpeg {logs} -i "{video}" -vf "{filters}" {output_frames}'
+        print(cmd)
+        os.system(cmd)
 
-def flir1_extract_frames(video, frames_dir):
+def flir1_extract_frames(video, frames_dir, verbose):
     # note: crop can sometimes be off by a pixel, so make sure it's exact
-    extract_frames(video, frames_dir, filters='format=gray, crop=240:238:54:15:exact=1')
+    extract_frames(video, frames_dir, 'format=gray, crop=240:238:54:15:exact=1', verbose)
 
-def navy_2015_extract_frames(video, frames_dir):
+def navy_2015_extract_frames(video, frames_dir, verbose):
     # note: crop can sometimes be off by a pixel, so make sure it's exact
-    extract_frames(video, frames_dir, filters='format=gray, crop=428:428:104:27:exact=1')
+    extract_frames(video, frames_dir, 'format=gray, crop=428:428:104:27:exact=1', verbose)
 
-def common_from_huggingface(dir, video_file, extract_frames_func):
+def common_from_huggingface(dir, video_file, extract_frames_func, verbose):
     video = f'{dir}/{video_file}'
     if not os.path.exists(video):
         import utils
         utils.download_from_huggingface(f'logicbear/uap/{video}')
-    extract_frames_func(video, dir)
+    extract_frames_func(video, dir, verbose)
     return FrameSequence(dir, get_fps(video))
 
-def flir1_from_huggingface():
-    return common_from_huggingface('flir1', '1 - FLIR.mp4', flir1_extract_frames)
+def flir1_from_huggingface(verbose = False):
+    return common_from_huggingface('flir1', '1 - FLIR.mp4', flir1_extract_frames, verbose)
 
-def gimbal_from_huggingface():
-    return common_from_huggingface('gimbal', '2 - GIMBAL.wmv', navy_2015_extract_frames)
+def gimbal_from_huggingface(verbose = False):
+    return common_from_huggingface('gimbal', '2 - GIMBAL.wmv', navy_2015_extract_frames, verbose)
 
-def gofast_from_huggingface():
-    return common_from_huggingface('gofast', '3 - GOFAST.wmv', navy_2015_extract_frames)
+def gofast_from_huggingface(verbose = False):
+    return common_from_huggingface('gofast', '3 - GOFAST.wmv', navy_2015_extract_frames, verbose)
 
 # interpolate values in a dataframe's fields around the switch from WH to BH
 def gimbal_fix_wh_to_bh(df, fields, minus = 10, plus = 0):
